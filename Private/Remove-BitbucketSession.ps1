2e31058b84238195462c1b3c4352d96bef7ae0f5
José Oliveira <jose.oliveira@sbanken.no>
2020-10-19 11:47:13 +0100

    fixing Remove-BitbucketSession

--- Private/Remove-BitbucketSession.ps1
+++ Private/Remove-BitbucketSession.ps1
@@ -2,6 +2,6 @@ Function Remove-BitbucketSession{
     param([Parameter(Mandatory=$true)] [String] $Id)
 
     $global:BITBUCKETCLI_SESSIONS =  $env:BITBUCKETCLI_SESSIONS `
-    | Where-Object { $_.Id -notlike "$Id" }
+    | Where-Object { $_.Id -eq $Id }
  
 }
