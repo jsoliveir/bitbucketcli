1166c68d1017f2ab4e601823f29cacbe6c90ebe0
José Oliveira <jose.oliveira@sbanken.no>
2020-10-19 11:30:34 +0100

    updating unit tests

--- tests/Add-BitbucketSession.Tests.ps1
+++ tests/Add-BitbucketSession.Tests.ps1
@@ -5,12 +5,12 @@ Describe "Add-BitbucketSession" {
   Context "environment_variables" {
     It "must_be_set" {
       Remove-Variable -Scope Global BITBUCKETCLI_SESSIONS -ErrorAction Ignore
-      Add-BitbucketSession -Server "test" -Username "test" -Password $("pwd"|ConvertTo-SecureString -AsPlainText -Force)
+      Add-BitbucketSession -Server "test" -Version "1" -Username "test" -Password $("pwd"|ConvertTo-SecureString -AsPlainText -Force)
         @($global:BITBUCKETCLI_SESSIONS).Count | Should -BeExactly 1 
     }
     It "must_be_valid" {
       Remove-Variable -Scope Global BITBUCKETCLI_SESSIONS -ErrorAction Ignore
-      Add-BitbucketSession -Server "test" -Username "test" -Password $("pwd"|ConvertTo-SecureString -AsPlainText -Force)
+      Add-BitbucketSession -Server "test" -Version "1" -Username "test" -Password $("pwd"|ConvertTo-SecureString -AsPlainText -Force)
       $global:BITBUCKETCLI_SESSIONS[0].Active | Should -Be $true 
       $global:BITBUCKETCLI_SESSIONS[0].Username | Should -Be "test" 
       $global:BITBUCKETCLI_SESSIONS[0].Password | Should -BeOfType [String]
@@ -19,8 +19,8 @@ Describe "Add-BitbucketSession" {
   Context "sessions" {
     It "must_be_only_one_active" {
       Remove-Variable -Scope Global BITBUCKETCLI_SESSIONS -ErrorAction Ignore
-      Add-BitbucketSession -Server "test" -Username "test" -Password $("pwd"|ConvertTo-SecureString -AsPlainText -Force)
-      Add-BitbucketSession -Server "test2" -Username "test2" -Password $("pwd"|ConvertTo-SecureString -AsPlainText -Force)
+      Add-BitbucketSession -Server "test" -Version "1" -Username "test" -Password $("pwd"|ConvertTo-SecureString -AsPlainText -Force)
+      Add-BitbucketSession -Server "test2" -Version "1" -Username "test2" -Password $("pwd"|ConvertTo-SecureString -AsPlainText -Force)
       @($global:BITBUCKETCLI_SESSIONS).Count | Should -BeExactly 2
       @($global:BITBUCKETCLI_SESSIONS | Where-Object Active -eq $true).Count | Should -BeExactly 1 
     }
@@ -28,7 +28,7 @@ Describe "Add-BitbucketSession" {
   Context "passwords" {
     It "must_be_secured_string" {
       Remove-Variable -Scope Global BITBUCKETCLI_SESSIONS -ErrorAction Ignore
-      Add-BitbucketSession -Server "test" -Username "test" -Password $("pwd"|ConvertTo-SecureString -AsPlainText -Force)
+      Add-BitbucketSession -Server "test" -Version "1" -Username "test" -Password $("pwd"|ConvertTo-SecureString -AsPlainText -Force)
       {ConvertTo-SecureString $global:BITBUCKETCLI_SESSIONS[0].Password} | Should -Not -Throw 
       {ConvertTo-SecureString $global:BITBUCKETCLI_SESSIONS[0].Password} | Should -Not -BeNullOrEmpty
     }
