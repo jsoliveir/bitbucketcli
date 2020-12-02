00dee52e94ba1ac910de7ec236fe5b0b1ca77a8a
José Oliveira <jose.oliveira@sbanken.no>
2020-12-02 14:27:35 +0000

    changed Get-BitbucketCloudDiffState From/To arguments

--- Public/Cloud/Get-BitbucketCloudDiffState.ps1
+++ Public/Cloud/Get-BitbucketCloudDiffState.ps1
@@ -4,11 +4,11 @@ Function Get-BitbucketCloudDiffState{
     param([Parameter(Mandatory=$false)] $Session = (Get-BitbucketSession),
           [Parameter(Mandatory=$true)] [String] $Workspace,
           [Parameter(Mandatory=$true)] [String] $Repository,
-          [Parameter(Mandatory=$true)] [String] $FromCommit,
-          [Parameter(Mandatory=$true)] [String] $ToCommit
+          [Parameter(Mandatory=$true)] [String] $From,
+          [Parameter(Mandatory=$true)] [String] $To
           )
     return (Invoke-RestMethod `
         -Method GET `
-        -Uri "$($Session.Server)/$($Session.Version)/repositories/$Workspace/$Repository/diffstat/$FromCommit..$ToCommit" `
+        -Uri "$($Session.Server)/$($Session.Version)/repositories/$Workspace/$Repository/diffstat/$From..$To" `
         -Headers @{ Authorization = $Session.Authorization}).values
 }
