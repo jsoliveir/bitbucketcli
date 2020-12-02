27456f243d75aa491e6a3138df9b02ebd2e6841d
José Oliveira <jose.oliveira@sbanken.no>
2020-12-02 20:26:59 +0000

    testing tests output format JUnitXml in bitbucket-pipelines

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -1,4 +1,4 @@
 Install-Module Pester -Force -SkipPublisherCheck
 Import-Module Pester -MinimumVersion 5.0.4 -Force
 New-Item -ItemType Directory test-results -Force
-Invoke-Pester -OutputFormat  NUnitXml -OutputFile "./test-results/results.xml"
+Invoke-Pester -OutputFormat  JUnitXml -OutputFile "./test-results/results.xml"
