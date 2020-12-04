497a7dbfe3a5c52647d29928907382747669611d
José Oliveira <jose.oliveira@sbanken.no>
2020-12-04 13:13:21 +0000

    removed parameter not supported page

--- Public/Cloud/Get-BitbucketCloudContent.ps1
+++ Public/Cloud/Get-BitbucketCloudContent.ps1
@@ -6,29 +6,21 @@ Function Get-BitbucketCloudContent{
           [Parameter(Mandatory=$true)] [String] $Repository,
           [Parameter(Mandatory=$true)] [String] $Path,
           [Parameter(Mandatory=$true)] [String] $Branch,
-          [Parameter(Mandatory=$false)] $Page,
-          [Parameter(Mandatory=$false)] $PageLen,
-          [Parameter(Mandatory=$false)] $Limit=1000
+          [Parameter(Mandatory=$false)] [String] $Query,
+          [Parameter(Mandatory=$false)] [Int] $PageLen=100
     )
 
     $Branch = $Branch -replace "refs/heads/",""
-
-    if($Branch -like $null){
-        $Branch = (Get-BitbucketCloudBranches -Worspace $Workspace -Repository $Repository `
-        | Sort-Object LastCommit -Descending `
-        | Select-Object -First 1).Name
-    }
-
     $Path = $Path -replace "\\","/"
     $Path = $Path -replace "^/",""
     $Path = $Path -replace "^\.\/",""
 
     $request = Invoke-RestMethod `
-        -Uri "$($Session.Server)/$($Session.Version)/repositories/${Workspace}/${Repository}/src/${Branch}/${Path}?page=${Page}&pagelen=${PageLen}&limit=${Limit}" `
+        -Uri "$($Session.Server)/$($Session.Version)/repositories/${Workspace}/${Repository}/src/${Branch}/${Path}?q=${Query}&pagelen=${PageLen}" `
         -Headers @{Authorization = $Session.Authorization } 
 
-    if($request.Values){
-        return $request.Values
+    if($request.values){
+        return $request.values
     }else{
         return $request
     }
