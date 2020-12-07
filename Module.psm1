d3924aa0f77c95a5ef074c1dff5244bfb3180d51
José Oliveira <jose.oliveira@sbanken.no>
2020-12-07 18:05:40 +0000

    removed unecessary comments

--- Module.psm1
+++ Module.psm1
@@ -6,13 +6,7 @@
 $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\ -Filter *.ps1 -Recurse -ErrorAction SilentlyContinue -Verbose:$false )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue -Verbose:$false )
 
-if($VerbosePreference){
-    Write-Host -ForegroundColor cyan "Importing Bitbucket Cloud CLI ..."
-}
 $Public | Sort-Object -Property Basename | Foreach-Object{
-    if($VerbosePreference){
-        Write-Host -ForegroundColor Magenta "* $($_.Basename)"
-    }
     . $_.FullName
 }
 $Private | Sort-Object -Property Basename | Foreach-Object{
