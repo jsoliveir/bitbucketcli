533028a4c89a2f9e2d6830af123489aaf640b600
José Oliveira <jose.oliveira@sbanken.no>
2020-12-09 11:26:50 +0000

    added new function New-BitbucketCloudTag

---
+++ Public/Cloud/New-BitbucketCloudTag.ps1
@@ -0,0 +1,16 @@
+
+Function New-BitbucketCloudTag {
+    param([Parameter(Mandatory=$false)] $Session = (Get-BitbucketSession),
+          [Parameter(Mandatory=$true)] [String] $Workspace,
+          [Parameter(Mandatory=$true)] [String] $Repository,
+          [Parameter(Mandatory=$true)] [String] $TagName,
+          [Parameter(Mandatory=$true)] [String] $CommitHash)
+    return Invoke-RestMethod `
+    -Method POST `
+    -Uri "$($Session.Server)/$($Session.Version)/repositories/$Workspace/$Repository/refs/tags" `
+    -Body "{ `"name`":`"$TagName`", `"target`": { `"hash`":`"$CommitHash`"}  }" `
+    -Headers @{
+        "Content-Type"= "application/json"
+        Authorization = $Session.Authorization 
+    }
+}
