6f6772ee3e34e0cc9a7f1a97193305f9e7fd0c71
José Oliveira <jose.oliveira@sbanken.no>
2020-12-10 12:44:25 +0000

    added new argument Revision

--- Public/Cloud/Get-BitbucketCloudCommits.ps1
+++ Public/Cloud/Get-BitbucketCloudCommits.ps1
@@ -2,11 +2,11 @@ Function Get-BitbucketCloudCommits{
     param([Parameter(Mandatory=$false)] $Session = (Get-BitbucketSession),
           [Parameter(Mandatory=$true)] [String] $Workspace,
           [Parameter(Mandatory=$true)] [String] $Repository,
-          [Parameter(Mandatory=$false)] [String] $Branch,
+          [Parameter(Mandatory=$false)] [String] $Revision,
           [Parameter(Mandatory=$false)] [int] $Limit = 1000 )
 
     return (Invoke-RestMethod `
         -Headers @{Authorization = $Session.Authorization } `
-        -Uri "$($Session.Server)/$($Session.Version)/repositories/${Workspace}/${Repository}/commits/${Branch}?limit=$Limit").values
+        -Uri "$($Session.Server)/$($Session.Version)/repositories/${Workspace}/${Repository}/commits/${Revision}?limit=$Limit").values
           
 }
