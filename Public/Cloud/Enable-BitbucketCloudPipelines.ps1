3ae918efc63ccd8e0ee2f2b974646f54671af0db
José Oliveira <jose.oliveira@sbanken.no>
2020-12-16 21:32:23 +0000

    changed parameter name

--- Public/Cloud/Enable-BitbucketCloudPipelines.ps1
+++ Public/Cloud/Enable-BitbucketCloudPipelines.ps1
@@ -2,11 +2,11 @@
 Function Enable-BitbucketCloudPipelines{
     param([Parameter(Mandatory=$false)] $Session = (Get-BitbucketSession),
           [Parameter(Mandatory=$true)] $Workspace,
-          [Parameter(Mandatory=$true)] $RepositoryName)
+          [Parameter(Mandatory=$true)] $Repository)
     
     Invoke-RestMethod `
         -Method PUT `
-        -Uri "$($Session.Server)/$($Session.Version)/repositories/$Workspace/$RepositoryName/pipelines_config" `
+        -Uri "$($Session.Server)/$($Session.Version)/repositories/$Workspace/$Repository/pipelines_config" `
         -Headers @{ Authorization = $Session.Authorization} `
         -ContentType "application/json" `
         -Body '{ "enabled":"true" }' `
