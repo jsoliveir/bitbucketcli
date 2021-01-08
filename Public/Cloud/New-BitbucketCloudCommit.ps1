9b31e9b838b6b2218a3cf7230a0edae6a04f99e5
José Oliveira <jose.oliveira@sbanken.no>
2021-01-08 13:14:24 +0000

    fixing new-BitbucketCloudCommit.ps1

--- Public/Cloud/New-BitbucketCloudCommit.ps1
+++ Public/Cloud/New-BitbucketCloudCommit.ps1
@@ -2,8 +2,8 @@ Function New-BitbucketCloudCommit{
     param([Parameter(Mandatory=$false)] [PSCustomObject] $Session = (Get-BitbucketSession),
           [Parameter(Mandatory=$true)]  [String]    $Workspace,
           [Parameter(Mandatory=$true)]  [String]    $Repository,
-          [Parameter(Mandatory=$false)] [String[]]  $FilesToDelete, 
-          [Parameter(Mandatory=$false)] [String[]]  $FilesToAdd,
+          [Parameter(Mandatory=$false)] [String]    $Path,
+          [Parameter(Mandatory=$false)] [String]    $Content,
           [Parameter(Mandatory=$false)] [String]    $Message,
           [Parameter(Mandatory=$false)] [String]    $Author,
           [Parameter(Mandatory=$false)] [String]    $Branch)
@@ -19,5 +19,6 @@ Function New-BitbucketCloudCommit{
             message = $Message
             branch = $Branch
             author = $Author
+            $Path  = $Content
          })
 }
