0198b5703936744725098bbe4e17d9b8751dcbd4
Oliveira, José <Jose.Oliveira@sbanken.no>
2021-01-10 16:43:09 +0000

    changed variable name

--- Public/Cloud/Get-BitbucketCloudContent.ps1
+++ Public/Cloud/Get-BitbucketCloudContent.ps1
@@ -5,18 +5,18 @@ Function Get-BitbucketCloudContent{
           [Parameter(Mandatory=$true)] [String] $Workspace,
           [Parameter(Mandatory=$true)] [String] $Repository,
           [Parameter(Mandatory=$true)] [String] $Path,
-          [Parameter(Mandatory=$false)] [String] $Branch="master",
+          [Parameter(Mandatory=$false)] [String] $Commit="master",
           [Parameter(Mandatory=$false)] [String] $Query,
           [Parameter(Mandatory=$false)] [Int] $PageLen=100
     )
 
-    $Branch = [Uri]::EscapeDataString($Branch -replace "refs/heads/","")
+    $Commit = [Uri]::EscapeDataString($Commit -replace "refs/heads/","")
     $Path = $Path -replace "\\","/"
     $Path = $Path -replace "^/",""
     $Path = $Path -replace "^\.\/",""
 
     $request = (Invoke-WebRequest `
-        -Uri "$($Session.Server)/$($Session.Version)/repositories/${Workspace}/${Repository}/src/${Branch}/${Path}?q=${Query}&pagelen=${PageLen}" `
+        -Uri "$($Session.Server)/$($Session.Version)/repositories/${Workspace}/${Repository}/src/${Commit}/${Path}?q=${Query}&pagelen=${PageLen}" `
         -ContentType "application/json;charset=utf-8" `
         -UseBasicParsing `
         -Headers @{
