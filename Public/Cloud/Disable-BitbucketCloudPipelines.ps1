f27acdbe5d653da6587b702aee3c89d6b55b52eb
Oliveira, José <Jose.Oliveira@sbanken.no>
2021-01-22 19:52:31 +0000

    added new function Disable-BitbucketCloudPipelines

---
+++ Public/Cloud/Disable-BitbucketCloudPipelines.ps1
@@ -0,0 +1,14 @@
+
+Function Disable-BitbucketCloudPipelines{
+    param([Parameter(Mandatory=$false)] $Session = (Get-BitbucketSession),
+          [Parameter(Mandatory=$true)] $Workspace,
+          [Parameter(Mandatory=$true)] $Repository)
+    
+    Invoke-RestMethod `
+        -Method PUT `
+        -Uri "$($Session.Server)/$($Session.Version)/repositories/$Workspace/$Repository/pipelines_config" `
+        -Headers @{ Authorization = $Session.Authorization} `
+        -ContentType "application/json" `
+        -Body '{ "enabled":"false" }' `
+    | Out-Null
+}
