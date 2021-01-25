639d9b823fe0fcf5112ee5e4923d04274877fa41
Oliveira, José <Jose.Oliveira@sbanken.no>
2021-01-25 11:04:55 +0000

    removing url encoding

--- Public/Cloud/New-BitbucketCloudCommit.ps1
+++ Public/Cloud/New-BitbucketCloudCommit.ps1
@@ -1,31 +1,31 @@
 Function New-BitbucketCloudCommit{
-    param([Parameter(Mandatory=$false)] [PSCustomObject] $Session = (Get-BitbucketSession),
-          [Parameter(Mandatory=$true)]  [String]    $Workspace,
-          [Parameter(Mandatory=$true)]  [String]    $Repository,
-          [Parameter(Mandatory=$false)] [String]    $Path,
-          [Parameter(Mandatory=$false)] [String]    $Content,
-          [Parameter(Mandatory=$false)] [String]    $Message,
-          [Parameter(Mandatory=$false)] [String]    $Author,
-          [Parameter(Mandatory=$false)] [String]    $Branch="master",
-          [Parameter(Mandatory=$false)] [Switch]    $Delete
-          )
-          
-      $body = @{
-         message =$Message
-         branch = [Uri]::EscapeDataString($Branch)
-         author = $Author
-      };
-      
-      if($Delete) { $Content = $null}
+   param([Parameter(Mandatory=$false)] [PSCustomObject] $Session = (Get-BitbucketSession),
+         [Parameter(Mandatory=$true)]  [String]    $Workspace,
+         [Parameter(Mandatory=$true)]  [String]    $Repository,
+         [Parameter(Mandatory=$false)] [String]    $Path,
+         [Parameter(Mandatory=$false)] [String]    $Content,
+         [Parameter(Mandatory=$false)] [String]    $Message,
+         [Parameter(Mandatory=$false)] [String]    $Author,
+         [Parameter(Mandatory=$false)] [String]    $Branch="master",
+         [Parameter(Mandatory=$false)] [Switch]    $Delete
+         )
+         
+     $body = @{
+        message =$Message
+        branch = $Branch
+        author = $Author
+     };
+     
+     if($Delete) { $Content = $null}
 
-      if($Path){ $body.Add($Path, $Content) }
+     if($Path){ $body.Add($Path, $Content) }
 
-      return  (Invoke-RestMethod `
-         -Method POST `
-         -Uri "$($Session.Server)/$($Session.Version)/repositories/$Workspace/$Repository/src/" `
-         -Headers @{ 
-             Authorization = $Session.Authorization
-             ContentType ='application/x-www-form-urlencode'
-            } `
-         -Body $body)
+     return  (Invoke-RestMethod `
+        -Method POST `
+        -Uri "$($Session.Server)/$($Session.Version)/repositories/$Workspace/$Repository/src/" `
+        -Headers @{ 
+            Authorization = $Session.Authorization
+            ContentType ='application/x-www-form-urlencode'
+           } `
+        -Body $body)
 }
