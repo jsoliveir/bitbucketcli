d33c4391601cb0cbeb35a9eba1666a982896c807
Oliveira, José <Jose.Oliveira@sbanken.no>
2021-01-25 15:24:54 +0000

    improvements

--- Public/Cloud/New-BitbucketCloudEnvironment.ps1
+++ Public/Cloud/New-BitbucketCloudEnvironment.ps1
@@ -3,12 +3,19 @@ Function New-BitbucketCloudEnvironment {
     param([Parameter(Mandatory=$false)] $Session = (Get-BitbucketSession),
           [Parameter(Mandatory=$true)] [String] $Workspace,
           [Parameter(Mandatory=$true)] [String] $Repository,
-          [Parameter(Mandatory=$false)] [String] $Environment)
+          [Parameter(Mandatory=$true)] [String] $Environment,
+          [Parameter(Mandatory=$false)] [String] $Type = "test"
+          )
 
     return Invoke-RestMethod `
     -Method POST `
     -Uri "$($Session.Server)/$($Session.Version)/repositories/$Workspace/$Repository/environments/" `
-    -Body "{ `"name`": `"Repository`" }" `
+    -Body "{ 
+        `"name`": `"$Environment`",
+        `"environment_type`":{
+            `"name`":`"$Type`"
+         }
+    }" `
     -Headers @{
         "Content-Type"= "application/json"
         Authorization = $Session.Authorization 
