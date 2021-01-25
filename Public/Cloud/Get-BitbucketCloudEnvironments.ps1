a65b0971e0d60dad6b95df054e0dd9c18771ed7a
Oliveira, José <Jose.Oliveira@sbanken.no>
2021-01-25 14:11:38 +0000

    new function Get-BitbucketCloudEnvironments

---
+++ Public/Cloud/Get-BitbucketCloudEnvironments.ps1
@@ -0,0 +1,14 @@
+
+Function Get-BitbucketCloudEnvironments {
+    param([Parameter(Mandatory=$false)] $Session = (Get-BitbucketSession),
+          [Parameter(Mandatory=$true)] [String] $Workspace,
+          [Parameter(Mandatory=$true)] [String] $Repository)
+
+    return Invoke-RestMethod `
+    -Method GET `
+    -Uri "$($Session.Server)/$($Session.Version)/repositories/$Workspace/$Repository/environments/" `
+    -Headers @{
+        "Content-Type"= "application/json"
+        Authorization = $Session.Authorization 
+    }
+}
