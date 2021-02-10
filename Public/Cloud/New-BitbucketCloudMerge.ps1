a5f94311f968e2db14c8cb93e39c2be7731eaa9f
Oliveira, José <Jose.Oliveira@sbanken.no>
2021-02-10 18:06:38 +0000

    testing bitbucket pipelines

--- Public/Cloud/New-BitbucketCloudMerge.ps1
+++ Public/Cloud/New-BitbucketCloudMerge.ps1
@@ -5,7 +5,7 @@ Function New-BitbucketCloudMerge {
           [Parameter(Mandatory=$true)]  [String]$PullRequestId,
           [Parameter(Mandatory=$false)] [Switch] $CloseBranch=$true,
           [Parameter(Mandatory=$false)] [String] [ValidateSet('squash','merge-commit','fast_forward')] $MergeStrategy='merge_commit',
-          [Parameter(Mandatory=$false)] [String] $Message='Automatically merging pull request')
+          [Parameter(Mandatory=$false)] [String] $Message)
       
     $payload = [PSCustomObject]@{
         "close_source_branch" = $CloseBranch
