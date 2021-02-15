9382025e4615feedbf002208a285fd21d0e87331
Oliveira, José <Jose.Oliveira@sbanken.no>
2021-02-15 20:47:00 +0000

    fixing tests

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -2,7 +2,7 @@ Install-Module Pester -Force -SkipPublisherCheck
 
 Import-Module Pester -MinimumVersion 5.0.4 -Force
 
-Import-Module  *.psd1 -Force
+Import-Module ./BitbucketCLI.psd1 -Force
 
 New-Item -ItemType Directory test-results -Force
 
