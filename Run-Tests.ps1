31ed59aa7c1f1467df021e3f4f8a923512317deb
Oliveira, José <Jose.Oliveira@sbanken.no>
2021-02-15 20:32:31 +0000

    tests

--- Run-Tests.ps1
+++ Run-Tests.ps1
@@ -1,4 +1,9 @@
 Install-Module Pester -Force -SkipPublisherCheck
+
 Import-Module Pester -MinimumVersion 5.0.4 -Force
+
+Import-Module  *.psd1 -Force
+
 New-Item -ItemType Directory test-results -Force
+
 Invoke-Pester -OutputFormat  JUnitXml -OutputFile "./test-results/results.xml"
