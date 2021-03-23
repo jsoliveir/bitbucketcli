ea71176ef103e2db52c8bab57b4ac80449cee132
Oliveira, José <Jose.Oliveira@sbanken.no>
2021-03-23 12:08:41 +0000

    TLS1.2  as the main security protocol

--- BitbucketCLI.psm1
+++ BitbucketCLI.psm1
@@ -2,6 +2,7 @@
     Check the following BITBUCKET API documentation to develop new features:
     https://developer.atlassian.com/bitbucket/api/2/reference/resource/
 #>
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
 
 $Public  = @( Get-ChildItem -Path $PSScriptRoot\Public\ -Filter *.ps1 -Recurse -ErrorAction SilentlyContinue -Verbose:$false )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue -Verbose:$false )
