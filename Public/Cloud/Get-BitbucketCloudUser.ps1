168176895a634f753b6b9cc1d03bbd07c90e8184
Oliveira, José <Jose.Oliveira@sbanken.no>
2021-04-05 15:28:58 +0100

    improved Get-BitbucketCloudUser

--- Public/Cloud/Get-BitbucketCloudUser.ps1
+++ Public/Cloud/Get-BitbucketCloudUser.ps1
@@ -1,8 +1,24 @@
 
 Function Get-BitbucketCloudUser {
-    param([Parameter(Mandatory=$false)] [PSCustomObject] $Session = (Get-BitbucketSession))
-    
-    return Invoke-RestMethod `
+    param(
+        [Parameter(Mandatory=$false)] [PSCustomObject] $Session = (Get-BitbucketSession),
+        [Parameter(Mandatory=$false)] [String] $UUID
+    )
+
+    if(!$UUID){
+        return Invoke-RestMethod `
         -Headers @{Authorization = $Session.Authorization } `
         -Uri "$($Session.Server)/$($Session.Version)/user"
+    }
+    
+    try {
+     return Invoke-RestMethod `
+        -Headers @{Authorization = $Session.Authorization } `
+        -Uri "$($Session.Server)/$($Session.Version)/users/$UUID"
+    }catch{
+        return Invoke-RestMethod `
+        -Headers @{Authorization = $Session.Authorization } `
+        -Uri "$($Session.Server)/$($Session.Version)/teams/$UUID"
+    }
+ 
 }
